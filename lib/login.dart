efe9f224fb6b7cf7125ccd87b3fa5d3a0fc4ec5d
hoang <vuvanhoanguet51@gmail.com>
2023-05-13 12:55:44 +0700

    first_app

---
+++ lib/login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BasicLayout extends StatelessWidget {
+  const BasicLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
