fe612a460f292855dee5dabb50e01937619f5929
unknown <vuvanhoanguet51@gmail.com>
2023-06-03 08:32:36 +0700

    flutter app text

--- lib/pages/favorite_page.dart
+++ lib/pages/favorite_page.dart
@@ -29,6 +29,12 @@ void main() {
     (element) => element % 2 == 0,
   );
   print(b);
+
+  final color = Color.values;
+
+  final a = Color.red.name;
+  print(a); // red
+  print(color); // [Color.red,Color.green,Color.black]
 }
 
 int sum({required int a, required int b}) {
@@ -39,3 +45,45 @@ void info({required String name, int age = 12}) {
   print(name);
   print(age);
 }
+
+class Point {
+  final double x;
+  final double y;
+
+  Point({required this.x, required this.y});
+}
+
+class Vector extends Point {
+  final double z;
+
+  Vector({required super.x, required super.y, required this.z});
+}
+
+abstract class Vehicle {
+  void helloWorld(String name) {}
+}
+
+class Car extends Vehicle {
+  void helloWorld(String name) {}
+}
+
+class MocVehicle implements Vehicle {
+  @override
+  void helloWorld(String name) {
+    // TODO: implement helloWorld
+  }
+}
+
+// interface
+interface class Student {
+  void info({required String name}) {}
+}
+
+class Student1 implements Student {
+  @override
+  void info({required String name}) {
+    // TODO: implement info
+  }
+}
+
+enum Color { red, green, black };
